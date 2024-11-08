ca37b37ae99bd3daa0032fa8fa1f465b4535a8c7
DirghayuKumar <dirghayu1777@gmail.com>
2024-11-08 21:07:57 +0530

    "Added Gym Sync Branched Emailing System"

--- GymManagementDjango/settings.py
+++ GymManagementDjango/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,5 +133,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "gymsync.official@gmail.com"
-EMAIL_HOST_PASSWORD = "jfrk gvun pose qvoa"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
