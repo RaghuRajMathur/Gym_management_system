4a29da3650c5eeb41fcb0db4086f98f485dd45e8
RaghuRajMathur <raghurajwe@gmail.com>
2025-01-14 21:38:01 +0530

    Changed admin nav

--- GymManagementDjango/settings.py
+++ GymManagementDjango/settings.py
@@ -80,24 +80,24 @@ WSGI_APPLICATION = "GymManagementDjango.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
-
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'RyCAUGIYcIyJvVpEaGhseJBRMVQSXRvF',
-        'HOST': 'autorack.proxy.rlwy.net',
-        'PORT': '30953',
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
+# DATABASES = {
+#     "default": {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'RyCAUGIYcIyJvVpEaGhseJBRMVQSXRvF',
+#         'HOST': 'autorack.proxy.rlwy.net',
+#         'PORT': '30953',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
